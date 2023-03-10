59e87138030609d63e1d09f523ae7f8babdcc152
GasinskiN <119811719+GasinskiN@users.noreply.github.com>
2023-03-10 23:16:28 +0100

    remove unnecessary module and add some comments

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 const apiKey = process.env.OPENWEATHER_API_KEY;
 
 app.get("/", function(req, res){
@@ -10,24 +9,24 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
+//     ask the user to input city for which the api will search 
+    const query = req.body.city;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=" + apiKey;
 
-const query = req.body.city;
-const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=" + apiKey;
-
-https.get(url, function(response){
-    console.log(response.statusCode);
-
-    response.on("data", function(data){
-        const weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
-        const weatherDescription = weatherData.weather[0].description;
-        const weatherDescriptionText = "<p>The weather is currently " + weatherDescription + "</p>";
-        const tempDescription = "<h1>The temperature in "+ query +" is " + temp + " degrees Celcius</h1>";
-        const image ="<image src='http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png'>";
-        res.write(weatherDescriptionText);
-        res.write(tempDescription);
-        res.write(image);
-        res.send();
+    https.get(url, function(response){
+        console.log(response.statusCode);
+//     Get data from api and render it readably for the user
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const weatherDescriptionText = "<p>The weather is currently " + weatherDescription + "</p>";
+            const tempDescription = "<h1>The temperature in "+ query +" is " + temp + " degrees Celcius</h1>";
+            const image ="<image src='http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png'>";
+            res.write(weatherDescriptionText);
+            res.write(tempDescription);
+            res.write(image);
+            res.send();
 });
 });
 });
